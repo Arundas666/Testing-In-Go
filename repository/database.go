c07975c2706879f0358f6054f2302722467a9a8d
Arundas666 <arundas285@gmail.com>
2024-01-31 21:31:27 +0530

    product side testing completed

--- repository/database.go
+++ repository/database.go
@@ -7,11 +7,9 @@ import (
 )
 
 func ConnectDB() (*sql.DB, error) {
-
 	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=Arun@1435 dbname=postgres sslmode=disable")
 	if err != nil {
 		return nil, err
-		
 	}
 	return db, nil
 }
