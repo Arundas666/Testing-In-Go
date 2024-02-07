13f35b81aad411fbd3f53dd65d88c7b2d4b3ad76
Arundas666 <arundas285@gmail.com>
2024-02-07 11:36:16 +0530

    cicd updated

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"test/handlers"
 	"test/repository"
@@ -9,22 +10,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
 
 	db, err := repository.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	userRepo:=repository.NewUserRepository(db)
-	userUsecase:=usecase.NewRegisterUserUseCase(userRepo)
-	userHandler:=handlers.NewUserHandler(userUsecase)
+	userRepo := repository.NewUserRepository(db)
+	userUsecase := usecase.NewRegisterUserUseCase(userRepo)
+	userHandler := handlers.NewUserHandler(userUsecase)
 
 	router := gin.Default()
-	
+	fmt.Println("HEyy")
 	router.POST("/usersignup", userHandler.Register)
 	router.GET("/userlogin", userHandler.Login)
-	
 
 	router.Run(":8000")
 
